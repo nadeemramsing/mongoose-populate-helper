bc82ff5153c26ad9ea023d23d74a525579e44094
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-05-30 08:49:18 +0400

    mongoosePopulateHelper implemented.

---
+++ index.js
@@ -0,0 +1,57 @@
+const
+    _ = require('lodash'),
+    mongoose = require('mongoose');
+
+module.exports = function mongoosePopulateHelper(schema, configs) {
+    _.each(configs, function (config) {
+        config.targetModel = config.targetModel ? mongoose.modelSchemas[config.targetModel] : schema;
+
+        const newField = {
+            [config.targetField.name]: _.omit(config.targetField, 'name')
+        };
+
+        schema.add(newField);
+
+        schema.pre('save', function (done) {
+            const document = this;
+
+            async.waterfall([
+                selectSourceField,
+                populateSourceField,
+                assignTargetField
+            ], done);
+
+            function selectSourceField(next) {
+                if (!_.isNil(document[config.sourceField]))
+                    return next(null, document);
+
+                document.constructor
+                    .findById(document.id)
+                    .select(
+                        [config.sourceField]
+                            .concat(Object.keys(document.toObject()))
+                            .join(' ')
+                    )
+                    .exec(next);
+            }
+
+            function populateSourceField(document, next) {
+                if (document === null)
+                    return done();
+
+                if (!(document[config.sourceField] instanceof mongoose.Types.ObjectId))
+                    return next(null, document);
+
+                document.populate(config.sourceField, next)
+            }
+
+            function assignTargetField(document, next) {
+                if (document[config.sourceField] === null)
+                    return done();
+
+                document[config.targetField.name] = config.map ? config.map(document[config.sourceField]) : document[config.sourceField];
+                next();
+            }
+        });
+    });
+}
